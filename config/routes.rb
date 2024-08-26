c5ba40ca69e2e361e70d1d1479a452b7c3513dcf
151339900 <165742289+151339900@users.noreply.github.com>
2024-08-26 06:54:40 +0000

    model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users do
+    root to: 'homes#top'
+  end
    # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
